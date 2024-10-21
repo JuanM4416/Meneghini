8779db55f16a50c44c5e1e08bb1fc895d3faeadb
unknown <juanmanuelmeneghini@gmail.com>
2024-10-21 16:39:14 -0300

    lo sigo en casa

---
+++ Laboratiorio1FPGA.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use EE.STD_LOGIC_1164.ALL;
+
+entity circuito1 is
+port (SW1 : in std_logic;
+		SW2 : in std_logic;
+		SW3 : in std_logic;
+		LED : out std_logic;)
+		
+end circuito1;
+
+architecture arquitectura1 of circuito1 is
+begin
+	
+	LED <= (SW1 xnor (not SW2)) or ((not SW2) and SW3);
+	
+end arquitectura1;
+	
